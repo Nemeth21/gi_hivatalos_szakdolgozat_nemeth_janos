d0708881583cf944adb0627bdf36506b36d99187
János Németh <nemeth.janos21@gmail.com>
2025-04-24 21:33:32 +0200

    adjusting commit : Változtatok ezen a Us-s vonalon, hiszen Magyarországról indulna ez az egész.

--- gi_hivatalos_szakdolgozat/app/components/TopNavbar.tsx
+++ gi_hivatalos_szakdolgozat/app/components/TopNavbar.tsx
@@ -4,9 +4,9 @@
  const TopNavbar = () => {
 
    return (
-    
+
      <div className="w-full font-inter font-mono bg-black text-white text-center text-xs md:text-sm py-4 tracking-wide">
-       FREE SHIPPING FOR ALL U.S. ORDERS OVER  
+       FREE SHIPPING FOR ALL HUNGARY. ORDERS OVER  
        <span className="text-orange-500 text-lg font-bold mx-1">$75</span>
      </div>
 
